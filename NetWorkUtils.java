a444d83a8d6679fa14bb0b156c81ed4df2b5b691
VVictorWang <643705913@qq.com>
2017-06-08 15:38:54 +0800

    工具库

---
+++ NetWorkUtils.java
@@ -0,0 +1,52 @@
+ /*
+    * 获取给定url值的string型数据
+    * @param myurl
+    * */
+    public static String sendRequst(String myurl) {
+        HttpURLConnection connection = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            URL url = new URL(myurl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(8000);
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(8000);
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (Exception p) {
+            p.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+
+        }
+        return builder.toString();
+    }
+
+//判断系统网络是否可用
+    public static boolean isNetWorkAvailabe(Context context) {
+        if (context.checkCallingOrSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivityManager == null) {
+                Log.d("网络:", "不能得到系统网络服务");
+            } else {
+                NetworkInfo[] infos = connectivityManager.getAllNetworkInfo();
+                if (infos != null) {
+                    for (int i = 0; i < infos.length; i++) {
+                        if (infos[i].getState() == NetworkInfo.State.CONNECTED) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        Log.d("网络:", "网络不可用");
+        return false;
+    }
---
+++ SharedPreferenceUtils.java
@@ -0,0 +1,109 @@
+ 
+    private static final SharedPreferences getSharedPreference(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+ /**
+     * 设置SharedPreference 值,int型
+     *
+     * @param context
+     * @param key
+     * @param value
+     */
+    public static final boolean putIntValue(Context context, String key,
+                                            int value) {
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putInt(key, value);
+        boolean result = editor.commit();
+        if (!result) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 设置SharedPreference 值,string型
+     *
+     * @param context
+     * @param key
+     * @param value
+     */
+    public static final boolean putValue(Context context, String key,
+                                         String value) {
+        value = value == null ? "" : value;
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(key, value);
+        boolean result = editor.commit();
+        if (!result) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 设置SharedPreference 值,boolean型
+     *
+     * @param context
+     * @param key
+     * @param b1
+     */
+
+    public static final void putBooleanValue(Context context, String key,
+                                             boolean bl) {
+        SharedPreferences.Editor edit = getSharedPreference(context).edit();
+        edit.putBoolean(key, bl);
+        edit.commit();
+    }
+      /**
+     * 设置SharedPreference 值,long型
+     *
+     * @param context
+     * @param key
+     * @param b1
+     */
+     public static final boolean putLongValue(Context context, String key,
+                                             Long value) {
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putLong(key, value);
+        return editor.commit();
+    }
+
+    /**
+     * 移除SharedPreference
+     *
+     * @param context
+     * @param key
+     */
+    public static final void RemoveValue(Context context, String key) {
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.remove(key);
+        boolean result = editor.commit();
+        if (!result) {
+            Log.e("移除Shared", "save " + key + " failed");
+        }
+    }
+
+    /**
+     * 获取SharedPreference 值
+     *
+     * @param context
+     * @param key
+     * @return
+     */
+    public static final String getValue(Context context, String key) {
+        return getSharedPreference(context).getString(key, "");
+    }
+
+    public static final Boolean getBooleanValue(Context context, String key) {
+        return getSharedPreference(context).getBoolean(key, false);
+    }
+
+    
+
+    public static final int getIntValue(Context context, String key) {
+        return getSharedPreference(context).getInt(key, 0);
+    }
+
+    public static final long getLongValue(Context context, String key,
+                                          long default_data) {
+        return getSharedPreference(context).getLong(key, default_data);
+    }
